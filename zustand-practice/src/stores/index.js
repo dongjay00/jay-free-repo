e1e26211e7341fd41480badd1fcaf4e640a5b352
dongjay00 <cjswodlehdgn@gmail.com>
2022-04-24 18:05:45 +0900

    [fix]: decreasePopulation fix

--- zustand-practice/src/stores/index.js
+++ zustand-practice/src/stores/index.js
@@ -3,6 +3,9 @@ import create from "zustand";
 export const useStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  decreasePopulation: () => set((state) => ({ bears: state.bears - 1 })),
+  decreasePopulation: () =>
+    set((state) => ({
+      bears: state.bears > 0 ? state.bears - 1 : state.bears,
+    })),
   removeAllBears: () => set({ bears: 0 }),
 }));

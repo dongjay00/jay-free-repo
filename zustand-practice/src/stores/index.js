60d5c511cedec850e727fc9800ac73574d897ff8
dongjay00 <cjswodlehdgn@gmail.com>
2022-04-24 18:07:39 +0900

    [feat]: increasePopulation fix

--- zustand-practice/src/stores/index.js
+++ zustand-practice/src/stores/index.js
@@ -2,7 +2,10 @@ import create from "zustand";
 
 export const useStore = create((set) => ({
   bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  increasePopulation: () =>
+    set((state) => ({
+      bears: state.bears < 12 ? state.bears + 1 : state.bears,
+    })),
   decreasePopulation: () =>
     set((state) => ({
       bears: state.bears > 0 ? state.bears - 1 : state.bears,

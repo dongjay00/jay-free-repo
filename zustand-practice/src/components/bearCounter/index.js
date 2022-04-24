41e2670ef192cf294cbeae92537869f743fd1dfd
dongjay00 <cjswodlehdgn@gmail.com>
2022-04-24 18:11:17 +0900

    [feat]: add text 'fence is full'

--- zustand-practice/src/components/bearCounter/index.js
+++ zustand-practice/src/components/bearCounter/index.js
@@ -2,7 +2,12 @@ import { useStore } from "../../stores";
 
 function BearCounter() {
   const bears = useStore((state) => state.bears);
-  return <h1>{bears} bears around here ...</h1>;
+  return (
+    <>
+      <h1>{bears} bears around here ...</h1>
+      {bears === 12 ? <h3>Fence is full</h3> : null}
+    </>
+  );
 }
 
 export default BearCounter;
